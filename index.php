48874b96fa68353b55617d5e784abfa866d46f19
DalinOurn <92624804+DalinOurn@users.noreply.github.com>
2022-09-07 16:54:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <title>php</title>
  </head>
  <body>
-  echo <p>Hello world from PHP! Declarative pipeline!!</p> 
+ <?php echo <p>Hello world from PHP! Declarative pipeline!!</p> 
   
  </body>
 </html>
